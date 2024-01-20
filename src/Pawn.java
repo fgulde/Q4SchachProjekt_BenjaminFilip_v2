8ba1151f47a96c98f4995c9b837d48d72b0863f2
Filip Gulde <filip@gmx.de>
2024-01-20 20:12:58 +0100

    Rechtschreibfehler

--- src/Pawn.java
+++ src/Pawn.java
@@ -1,6 +1,6 @@
 // Spielfigur: Pawn / Bauer
 public class Pawn extends Piece {
-    boolean moved = false; // Checkt ob der Pawn schon bewegt wurde, da er im ersten Zug zwei Felder vorran kann
+    boolean moved = false; // Checkt ob der Pawn schon bewegt wurde, da er im ersten Zug zwei Felder voran kann
 
     public Pawn(boolean white, boolean killed, Tile position) {
         super(white, killed, position);
