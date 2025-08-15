cf7b87fb0584ba7ce3033fef269f3ffd8943ffd6
benja <benjamin.dembinski@student.tu-freiberg.de>
2025-08-15 20:50:17 +0200

    Remove commented-out move safety checks in Pawn and Piece classes

--- src/Piece.java
+++ src/Piece.java
@@ -532,15 +532,15 @@ public abstract class Piece {
         return false;
     }
 
-    public static boolean isMoveSafe(Piece piece, int newX, int newY) {
-        boolean isSafe;
-        Tile currentPosition = piece.getPosition();
-
-        piece.move(newX, newY, false);
-        isSafe = CheckCheck(piece.isWhite());
-        piece.move(currentPosition.getX(), currentPosition.getY(), false);
-        return isSafe;
-    }
+//    public static boolean isMoveSafe(Piece piece, int newX, int newY) {
+//        boolean isSafe;
+//        Tile currentPosition = piece.getPosition();
+//
+//        piece.move(newX, newY, false);
+//        isSafe = CheckCheck(piece.isWhite());
+//        piece.move(currentPosition.getX(), currentPosition.getY(), false);
+//        return isSafe;
+//    }
 
     @SuppressWarnings("BooleanMethodIsAlwaysInverted")
     public static boolean stillGeneratingKillButton(Piece currentPiece){
